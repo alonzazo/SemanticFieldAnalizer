dd8ae2c0c9ad11947a10cf61ab3b0fdd8731f726
oscar.azofeifa.segura@gmail.com <oscar.azofeifa.segura@gmail.com>
2018-05-09 00:25:26 -0600

    Primer commit

---
+++ SemanticFieldAnalizer/Views/FrmMain.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace SemanticFieldAnalizer
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnAnalize = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnBrowseDiccionary = new System.Windows.Forms.Button();
+            this.TxtDictionaryPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtTextPath = new System.Windows.Forms.TextBox();
+            this.BtnBrowseText = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.TxtConsole = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.BtnAnalize, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.07937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.92064F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(550, 315);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // BtnAnalize
+            // 
+            this.BtnAnalize.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnAnalize.Enabled = false;
+            this.BtnAnalize.Location = new System.Drawing.Point(3, 270);
+            this.BtnAnalize.Name = "BtnAnalize";
+            this.BtnAnalize.Size = new System.Drawing.Size(544, 42);
+            this.BtnAnalize.TabIndex = 1;
+            this.BtnAnalize.Text = "Analizar";
+            this.BtnAnalize.UseVisualStyleBackColor = true;
+            this.BtnAnalize.Click += new System.EventHandler(this.BtnAnalize_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(544, 261);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(538, 124);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Requerimientos";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.46324F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.41912F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.93382F));
+            this.tableLayoutPanel2.Controls.Add(this.btnBrowseDiccionary, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.TxtDictionaryPath, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.TxtTextPath, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnBrowseText, 2, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(8, 19);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.35353F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.35353F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.28283F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(530, 99);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // btnBrowseDiccionary
+            // 
+            this.btnBrowseDiccionary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseDiccionary.Location = new System.Drawing.Point(431, 41);
+            this.btnBrowseDiccionary.Name = "btnBrowseDiccionary";
+            this.btnBrowseDiccionary.Size = new System.Drawing.Size(96, 23);
+            this.btnBrowseDiccionary.TabIndex = 5;
+            this.btnBrowseDiccionary.Text = "Examinar...";
+            this.btnBrowseDiccionary.UseVisualStyleBackColor = true;
+            this.btnBrowseDiccionary.Click += new System.EventHandler(this.btnBrowseDiccionary_Click);
+            // 
+            // TxtDictionaryPath
+            // 
+            this.TxtDictionaryPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtDictionaryPath.Location = new System.Drawing.Point(95, 42);
+            this.TxtDictionaryPath.Name = "TxtDictionaryPath";
+            this.TxtDictionaryPath.Size = new System.Drawing.Size(330, 20);
+            this.TxtDictionaryPath.TabIndex = 4;
+            this.TxtDictionaryPath.Leave += new System.EventHandler(this.verifyTextBoxes);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 39);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(61, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ruta del diccionario:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 4);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(73, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ruta del texto objetivo:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TxtTextPath
+            // 
+            this.TxtTextPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtTextPath.Location = new System.Drawing.Point(95, 7);
+            this.TxtTextPath.Name = "TxtTextPath";
+            this.TxtTextPath.Size = new System.Drawing.Size(330, 20);
+            this.TxtTextPath.TabIndex = 1;
+            this.TxtTextPath.Leave += new System.EventHandler(this.verifyTextBoxes);
+            // 
+            // BtnBrowseText
+            // 
+            this.BtnBrowseText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnBrowseText.Location = new System.Drawing.Point(431, 6);
+            this.BtnBrowseText.Name = "BtnBrowseText";
+            this.BtnBrowseText.Size = new System.Drawing.Size(96, 23);
+            this.BtnBrowseText.TabIndex = 2;
+            this.BtnBrowseText.Text = "Examinar...";
+            this.BtnBrowseText.UseVisualStyleBackColor = true;
+            this.BtnBrowseText.Click += new System.EventHandler(this.BtnBrowseText_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.TxtConsole);
+            this.groupBox2.Location = new System.Drawing.Point(3, 133);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(538, 125);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Consola";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // TxtConsole
+            // 
+            this.TxtConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtConsole.BackColor = System.Drawing.SystemColors.InfoText;
+            this.TxtConsole.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtConsole.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TxtConsole.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtConsole.ForeColor = System.Drawing.SystemColors.Info;
+            this.TxtConsole.Location = new System.Drawing.Point(6, 19);
+            this.TxtConsole.Multiline = true;
+            this.TxtConsole.Name = "TxtConsole";
+            this.TxtConsole.ReadOnly = true;
+            this.TxtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtConsole.Size = new System.Drawing.Size(526, 100);
+            this.TxtConsole.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 339);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmMain";
+            this.Text = "Analizador de campo semántico simple";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BtnAnalize;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnBrowseDiccionary;
+        private System.Windows.Forms.TextBox TxtDictionaryPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtTextPath;
+        private System.Windows.Forms.Button BtnBrowseText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox TxtConsole;
+    }
+}
+
---
+++ SemanticFieldAnalizer/Views/FrmMain.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemanticFieldAnalizer
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowseText_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = false;
+            
+            // Process input if the user clicked OK.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK ? true : false)
+            {
+                // Open the selected file to read.
+                TxtTextPath.Text = openFileDialog1.FileName;
+                checkConditions();
+            }
+        }
+
+        private void btnBrowseDiccionary_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "CSV Files (.csv)|*.csv|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = false;
+
+            // Process input if the user clicked OK.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK ? true : false)
+            {
+                // Open the selected file to read.
+                TxtDictionaryPath.Text = openFileDialog1.FileName;
+                checkConditions();
+            }
+        }
+
+        private bool verifyCSV() {
+            try {
+                string[] csv = System.IO.File.ReadAllLines(TxtDictionaryPath.Text);
+                int lineNum = 1;
+                foreach (string row in csv) {
+                    string[] words = row.Split(',');
+                    if (words.Length < 2) {
+                        TxtConsole.Text += "\r\n" + "EXCEPCIÓN: \tLinea " + lineNum + " no respeta estructura CSV.";
+                        TxtConsole.Text += "\r\n" + "SUGERENCIA: \tRevisar línea especificada.";
+                        TxtConsole.Text += "\r\n" + "DETALLE: \r\n" + row;
+                        MessageBox.Show("Estructura de CSV inválida. Revise el archivo.");
+                        TxtDictionaryPath.Text = "";
+                        return false;
+                    }
+                    lineNum++;
+                }
+                TxtConsole.Text += "\r\n" + "VERIFICACIÓN DE CSV EXITOSA";
+                return true;
+            }
+            catch (Exception ex) {
+                TxtConsole.Text += "\r\n" + "EXCEPCIÓN:\tLectura del diccionario";
+                TxtConsole.Text += "\r\n" + "SUGERENCIA:\tVerifique la ruta";
+                TxtConsole.Text += "\r\n" + "DETALLE:\n" + ex.ToString();
+                return false;
+            }
+            
+        }
+
+        private bool verifyText() {
+            try
+            {
+                string[] csv = System.IO.File.ReadAllLines(TxtTextPath.Text);
+               
+                TxtConsole.Text += "\r\n" + "VERIFICACIÓN DE RUTA DE TEXTO EXITOSA";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                TxtConsole.Text += "\r\n" + "EXCEPCIÓN:\tLectura del texto objetivo";
+                TxtConsole.Text += "\r\n" + "SUGERENCIA:\tVerifique la ruta";
+                TxtConsole.Text += "\r\n" + "DETALLE:\n" + ex.ToString();
+                return false;
+            }
+        }
+
+        private void checkConditions() {
+            if (verifyCSV() && verifyText())
+            {
+                BtnAnalize.Enabled = true;
+            }
+            else {
+                BtnAnalize.Enabled = false;
+            }
+        }
+
+        private void verifyTextBoxes(object sender, EventArgs e)
+        {
+            checkConditions();
+        }
+
+        private void BtnAnalize_Click(object sender, EventArgs e)
+        {
+            //MOSTRAR LOS RESULTADOS
+        }
+    }
+}
